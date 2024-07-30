8093297a687f9a83ea7c7fd0a2cfe46992349ea0
Harsh33kumar <141348484+Harsh33kumar@users.noreply.github.com>
2024-07-30 12:20:12 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,24 @@
+let buttons=document.querySelectorAll('button');
+let string="";
+let arr=Array.from(buttons);
+arr.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='C'){
+                string="";
+                document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='←'){
+            string=string.substring(0,string.length-1);
+            document.querySelector('input').value=string;
+        }
+        else{
+            console.log(e.target);
+            string=string+e.target.innerHTML;
+            document.querySelector('input').value=string;
+        }
+    })
+})
